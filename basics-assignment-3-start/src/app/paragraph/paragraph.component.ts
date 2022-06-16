6eeae33b8b0216d296163370129aa9e27bd0687a
andreybozhkov <bozhkov.andrey@gmail.com>
2022-06-16 15:25:37 +0300

    Updated basics assignment 3 solution

--- basics-assignment-3-start/src/app/paragraph/paragraph.component.ts
+++ basics-assignment-3-start/src/app/paragraph/paragraph.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'paragraph',
-    templateUrl: './paragraph.component.html'
+    template: '<p>Secret Password = tuna</p>'
 })
 export class ParagraphComponent {
     
